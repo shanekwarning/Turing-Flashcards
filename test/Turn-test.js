58bfe98db91ca0b3a4a0000563afbae6c462827b
Shane Warning <shane.k.warning@gmail.com>
2022-03-22 20:08:37 -0500

    Update test to have card instanitations

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -10,51 +10,54 @@ describe('Turn', () => {
 })
 
 it('Should take a users guess', () => {
-  const turn = new Turn(guess);
+  const turn = new Turn('pug');
 
-  expect(turn.guess).to.equal(guess)
+  expect(turn.guess).to.equal('pug')
 });
 
 it('Turn should have a card', () => {
-  const turn = new Turn(guess, card);
+  const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  const turn = new Turn('pug', card);
 
   expect(turn.card).to.equal(card);
 });
 
 it('should return a guess', () => {
-  const turn = new Turn(guess, card);
+  const turn = new Turn('pug');
 
-  turn.returnGuess();
-
-  expect(turn.returnGuess()).to.equal(guess)
+  expect(turn.returnGuess()).to.equal('pug')
 });
 
 it('should return a card', () => {
-  const turn = new Turn(guess, card);
-
-  turn.returnCard();
+  const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  const turn = new Turn('pug', card);
 
   expect(turn.returnCard()).to.equal(card)
 });
 
 it('should return if the answer is true', () => {
-  const turn = new Turn(guess, card);
-  const turn2 = new Turn(guess, card;)
-
+  const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  const card2 = new Card(2, 'What is Jill\'s favorite animal', ['lion', 'tiger', 'bear'], 'lion');
+  const turn = new Turn('sea otter', card);
+  const turn2 = new Turn('bear', card2);
   turn.evaluateGuess();
   turn2.evaluateGuess();
 
-  expect(turn.evaluate()).to.equal(true);
-  expect(turn2.evaluate()).to.equal(false)
+  expect(turn.evaluateGuess()).to.equal(true);
+  expect(turn2.evaluateGuess()).to.equal(false)
 });
 
 it('shoud give feedback', () => {
-  const turn = new Turn(guess, card);
-  const turn2 = new Turn(guess, card);
+  const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  const card2 = new Card(2, 'What is Jill\'s favorite animal', ['lion', 'tiger', 'bear'], 'lion');
+  const turn = new Turn('sea otter', card);
+  const turn2 = new Turn('bear', card2);
 
+  turn.evaluateGuess();
   turn.giveFeedback();
+  turn2.evaluateGuess();
   turn2.giveFeedback();
 
-  expect(turn.giveFeedback).to.equal('correct!');
-  expect(turn2.giveFeedback).to.equal('incorrect')
+  expect(turn.giveFeedback()).to.equal('correct!');
+  expect(turn2.giveFeedback()).to.equal('incorrect')
 })
