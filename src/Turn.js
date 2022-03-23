bde8743d1d8fcf58c60291d0104de9e3251d8fed
Shane Warning <shane.k.warning@gmail.com>
2022-03-22 20:08:59 -0500

    Complete turn file to pass test

---
+++ src/Turn.js
@@ -0,0 +1,31 @@
+class Turn {
+  constructor(guess, card) {
+  this.guess = guess,
+  this.card = card,
+  this.correct = undefined
+  }
+  returnGuess() {
+    return this.guess
+  };
+  returnCard() {
+    return this.card
+  };
+  evaluateGuess() {
+    if(this.guess === this.card.correctAnswer) {
+      this.correct = true
+      return true
+    } else{
+      this.correct = false
+      return false
+    }
+  }
+  giveFeedback() {
+    if(this.correct === true){
+      return 'correct!'
+    } else {
+      return 'incorrect'
+    }
+  }
+}
+
+module.exports = Turn
