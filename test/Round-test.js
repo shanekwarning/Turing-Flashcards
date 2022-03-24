43f3de22a200bf41a4d7d8e3003aeff77c60aed4
Shane Warning <shane.k.warning@gmail.com>
2022-03-24 18:35:32 -0500

    Implement before each for testing

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,45 +7,42 @@ const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 describe('Round', () => {
+  let round;
+  let round2;
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+  let deck2;
+
+  beforeEach(() => {
+     card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+     card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+     deck = new Deck([card1, card2, card3]);
+     deck2 = new Deck([card1, card2, card3]);
+
+     round = new Round(deck);
+     round2 = new Round(deck2);
+  });
 
   it('should be a function', function() {
-    const round = new Round();
+    // const round = new Round();
   expect(round).to.be.an.instanceof(Round)
 });
 
 it('should be able to have a Deck of Cards', () => {
-  const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-  const deck = new Deck([card1, card2, card3]);
-
-  const round = new Round(deck);
 
   expect(round.deck).to.equal(deck)
 });
 
 it('should be able to return the current Card', () =>{
-  const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
 
-  const deck = new Deck([card1, card2, card3]);
-
-  const round = new Round(deck);
   expect(round.returnCurrentCard()).to.equal(card1)
 });
 
 it('should start a new Turn when a guess is made', () => {
-  const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-  const deck = new Deck([card1, card2, card3]);
-  const deck2 = new Deck([card1, card2, card3]);
-
-  const round = new Round(deck);
-  const round2 = new Round(deck2);
 
   round.returnCurrentCard()
   round2.returnCurrentCard()
@@ -57,15 +54,6 @@ it('should start a new Turn when a guess is made', () => {
 });
 
 it('should keep track of incorrect guesses', () => {
-  const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-  const deck = new Deck([card1, card2, card3]);
-  const deck2 = new Deck([card1, card2, card3]);
-
-  const round = new Round(deck);
-  const round2 = new Round(deck2);
 
   round.returnCurrentCard();
   round2.returnCurrentCard();
@@ -78,14 +66,7 @@ it('should keep track of incorrect guesses', () => {
   });
 
   it('should be able to calculate the percent of corect answers', () => {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
-    const deck = new Deck([card1, card2, card3]);
-
-    const round = new Round(deck);
-
+  
     round.returnCurrentCard();
     round.takeTurn('sea otter');
     round.returnCurrentCard();
