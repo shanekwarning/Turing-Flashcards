7604c239d1c5ea687cb03a04489a4e06298462c3
Shane Warning <shane.k.warning@gmail.com>
2022-03-23 22:28:24 -0500

    Add key value and ability to have percent print to console

--- src/Round.js
+++ src/Round.js
@@ -3,7 +3,8 @@ class Round {
     this.deck = deck,
     this.currentCard = undefined,
     this.turns = 0,
-    this.incorrectGuesses = []
+    this.incorrectGuesses = [],
+    this.correctPercent = 100
   }
   returnCurrentCard() {
     this.currentCard = this.deck.cards.shift();
@@ -17,10 +18,20 @@ class Round {
     if(guess !== this.currentCard.correctAnswer) {
       this.incorrectGuesses.push(this.currentCard.id)
     };
-  
+
     turn.evaluateGuess();
 
     return turn.giveFeedback()
+  }
+  calculatePercentCorrect() {
+    const incorrectPercent = 100*(this.turns - this.incorrectGuesses.length);
+
+    this.correctPercent = incorrectPercent/this.turns;
+
+    return this.correctPercent
+  }
+  endRound() {
+    console.log(`**Round over!** You answered ${this.correctPercent}% of the questions correctly!`)
   }
 }
 
