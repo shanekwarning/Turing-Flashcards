40610d17f055f44e64bd852fed155da29b750555
Shane Warning <shane.k.warning@gmail.com>
2022-03-23 22:00:06 -0500

    Create way to keep track of incorrect guesses

--- src/Round.js
+++ src/Round.js
@@ -1,17 +1,25 @@
 class Round {
   constructor(deck) {
     this.deck = deck,
-    this.currentCard = undefined
-    this.turns = 0
+    this.currentCard = undefined,
+    this.turns = 0,
+    this.incorrectGuesses = []
   }
   returnCurrentCard() {
     this.currentCard = this.deck.cards.shift();
     return this.currentCard
   }
   takeTurn(guess) {
-    this.turns++
+    this.turns++;
+
     const turn = new Turn(guess, this.currentCard);
-    turn.evaluateGuess()
+
+    if(guess !== this.currentCard.correctAnswer) {
+      this.incorrectGuesses.push(this.currentCard.id)
+    };
+  
+    turn.evaluateGuess();
+
     return turn.giveFeedback()
   }
 }
